731aa48cd306a5bdcd73883cbdfeccc8064d87e3
Mauricio Rodrigues Coelho <47572310+mauriciocoelho@users.noreply.github.com>
2022-03-16 16:01:08 -0300

    Update and rename edit.blade.php to create.blade.php

--- create.blade.php
+++ create.blade.php
@@ -1,26 +1,25 @@
-<form action="{{ route('users.update', $user->id) }}" method="post" enctype="multipart/form-data">
-    {{ method_field('patch') }}
+<form action="{{route('users.store')}}" method="post" enctype="multipart/form-data">
     {{ csrf_field() }}
-    <div class="modal fade text-left" id="ModalEdit{{$user->id}}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal fade text-left" id="ModalCreate" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">{{ __('Edit User') }}</h4>
+                    <h4 class="modal-title">{{ __('Create New User') }}</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                    </button>
+                    </button>                    
                 </div>
                 <div class="modal-body">
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>{{ __('Name') }}:</strong>
-                            {!! Form::text('name', $user->name, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>{{ __('Email') }}:</strong>
-                            {!! Form::text('email', $user->email, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12">
@@ -38,11 +37,12 @@
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>{{ __('Role') }}:</strong>
-                            {!! Form::select('roles[]', $roles,$user->userRole->name, array('class' => 'form-control','multiple')) !!}
+                            {!! Form::select('roles[]', $roles,[], array('class' => 'form-control','multiple')) !!}
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12">
-                        <button type="submit" class="btn btn-warning">{{ __('Edit') }}</button>
+                        <button type="button" class="btn grey btn-outline-secondary" data-dismiss="modal">{{ __('Back') }}</button>
+                        <button type="submit" class="btn btn-success">{{ __('Save') }}</button>
                     </div>
                 </div>
             </div>
